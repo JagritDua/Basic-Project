c2cc054e12569ce0f67c16048da40fe400c9a484
Jagrit Dua <jagrit.dua@ukg.com>
2025-08-20 14:24:53 +0530

    testing3

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello Hello Hello4"
 
+@app.route("/testing2")
+def home2():
+    return "testing 2"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
